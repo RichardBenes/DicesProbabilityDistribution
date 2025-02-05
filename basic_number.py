bdd9a9a71cf4f14c24af6e4b35102b7347f33684
Richard Beneš <richard.benes.9@email.cz>
2025-02-05 18:38:55 +0100

    Trying to measure what takes long

--- basic_number.py
+++ basic_number.py
@@ -1,7 +1,15 @@
 import random as rnd
 import matplotlib.pyplot as plt
+from time import time
 
+start = time()
 results = [rnd.randint(1,6) for _ in range(100000)]
+stop = time()
 
 plt.hist(results)
+
+stop2 = time()
+
+print(f"Generating results took {stop - start}, rendering {stop2 - stop}.")
+
 plt.show()
